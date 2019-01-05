96073f16beda292c3e43d00fc24b878f2f51a14d
joocer <justin.joyce@joocer.com>
2019-01-05 10:31:42 +0000

    forward execution implemented

--- classes/brain.js
+++ classes/brain.js
@@ -1,5 +1,5 @@
 class brain {
-    constructor(inputNeuronCount, outputNeuronCount) {
+    constructor(inputNeuronCount, outputNeuronCount) { // todo: change to be brain({ inputSize: 3, hiddenLayers[ { neurons: 4 }, { neurons: 4 } ], outputSize: 2 })
         //todo: loadWeights()
         //todo: saveWeights()
         
@@ -31,21 +31,28 @@ class brain {
 
         // execute the model and return all of the values
         this.executeFullResults = function(inputValues) {
+            // load the input layer (layer 0) with the inputValues
+            for (var i = 0; i < this.Layers[0].length; i++) {
+                this.Layers[0][i].activationEnergy = inputValues[i];
+            }
+//console.log(this.Layers[0]);
+
+            // trigger all the synapses
             for (var l = 0; l < (this.Layers.length - 1); l++) {            // source layer
-                for (var t = 0; t < this.Layers[l + 1].length; t++) {
-                for (var s = 0; s < this.Layers[l].length; s++) {           // source node
-                       // target node
-console.log('add energy to [', l + 1, ',', t, '] from [', l, ',', s, ']');
-                        this.Layers[l + 1][t].addInput(this.Layers[l][s], this.Synapses[l][s][t]);
+                for (var t = 0; t < this.Layers[l + 1].length; t++) {       // target node
+                    for (var s = 0; s < this.Layers[l].length; s++) {       // source node                       
+//console.log('add energy to [', l + 1, ',', t, '] from [', l, ',', s, ']');
+                        this.Layers[l + 1][t].addInput(this.Layers[l][s].activationEnergy, this.Synapses[l][s][t]);
                     }
-//console.log('#', l, s, t);
-                    //this.Layers[l + 1][t].fire();
+//console.log('fire neuron [', l + 1, ',', t, ']');
+                    this.Layers[l + 1][t].fire();
                 }
             }
 
+            // extract the output layer
             var result = [];
             for (var i = 0; i < this.Layers[this.Layers.length - 1].length; i++) {
-                result.push(this.Layers[this.Layers.length - 1][i].fire());
+                result.push(this.Layers[this.Layers.length - 1][i].activationEnergy);
             }
             return result;
         }
@@ -96,6 +103,8 @@ class neuron {
     constructor() {
         // the combined value of the inputs
         this.energy = 0;
+        // the result of the neuron being fired
+        this.activationEnergy = 0;
         // only when this is met does the neuron 'fire'
         this.threshold = 0.01;
         // this is the 'compression' algorithm - default to sigmoid
@@ -103,14 +112,17 @@ class neuron {
         // when neurons in previous layer fire, call this
         this.addInput = function(value, strength) {
             this.energy += this.energy + (value * strength);
+//console.log ('adding energy ', { 'value': value, 'strength': strength, 'total energy': this.energy });
         }
         // when all previous neurons have fired and this neuron's energy calculated, fire this neuron
         // returns the power to forward on
         this.fire = function() {
             var activation = this.nonlinearityfunction(this.energy);
             if (activation < this.threshold) { activation = null } 
-            return activation 
+            this.activationEnergy = activation;
+console.log ('firing ', { 'energy': this.energy, 'activation': activation });
         }
+
     }
 }
 
@@ -137,8 +149,8 @@ function tanh(t) {
 var myBrain = new brain(3,2);
 myBrain.addHiddenLayer(4);
 myBrain.initialize();
-myBrain.train([0,0,0], [0,1]);
-myBrain.train([0,0,1], [1,0]);
-myBrain.train([0,1,0], [1,0]);
+//myBrain.train([0,0,0], [0,1]);
+//myBrain.train([0,0,1], [1,0]);
+//myBrain.train([0,1,0], [1,0]);
 
 console.log(myBrain.execute([1,1,1]));
